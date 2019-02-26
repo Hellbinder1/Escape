d4f0741f6f6fa87b70574abc87054f32fcbabe5a
SUSSEXDOWNS\459872 <459872@chrome.sussexdowns.ac.uk>
2019-02-26 16:31:47 +0000

    26/02/19 Switching Door and Key to Raycast - WIP

---
+++ Escape Game/Assets/Scripts/RaycastSelect.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaycastSelect : MonoBehaviour
+{
+
+    public float selectionDistance = 1f;
+    public LayerMask layermask;
+
+    private GameObject currentTarget;
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(transform.position, transform.forward, out hit, selectionDistance, layermask))
+        {
+            if (currentTarget == null)
+            {
+                currentTarget = hit.transform.gameObject;
+                OnRaycastEnter(currentTarget);
+            }
+
+            else if (currentTarget != hit.transform.gameObject)
+            {
+                OnRaycastLeave(currentTarget);
+                currentTarget = hit.transform.gameObject;
+                OnRaycastEnter(currentTarget);
+            }
+        }
+
+        else if (currentTarget != null)
+        {
+            OnRaycastLeave(currentTarget);
+            currentTarget = null;
+        }
+
+    }
+
+    protected virtual void OnRaycastEnter(GameObject target)
+    {
+    }
+
+    protected virtual void OnRaycastLeave(GameObject target)
+    {
+    }
+
+    protected virtual void OnRaycast(GameObject target)
+    {
+    }
+}
